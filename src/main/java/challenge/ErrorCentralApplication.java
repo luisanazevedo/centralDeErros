f8d793001ed2e722af1ce4293910822aaddd570d
Luisa Azevedo <nunesazevedoluisa@gmail.com>
2020-05-25 23:50:31 -0300

    Projeto Criado

---
+++ src/main/java/challenge/ErrorCentralApplication.java
@@ -0,0 +1,13 @@
+package challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorCentralApplication.class, args);
+	}
+
+}
